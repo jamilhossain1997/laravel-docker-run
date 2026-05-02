9da2086928b91dea5d287df19e2640b145cd091a
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-05-02 16:13:57 +0600

    Migrate careate atble

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,19 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class Project extends Model
 {
 
-    public function up()
-    {
-        Schema::create('projects', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('version');
-            $table->timestamps();
-        });
-    }
+    protected $fillable = ['name', 'version', 'description']; 
 }
